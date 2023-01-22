6c7b35c890361b0966531756c25c8150166d3849
iMasanari <14dd023@gmail.com>
2023-01-22 15:20:05 +0900

    リダイレクト用ページを作成

---
+++ script.mjs
@@ -0,0 +1,29 @@
+import { readFile, writeFile, mkdir } from 'fs/promises'
+import { join, dirname } from 'path'
+
+const template = (path) =>
+  `<!DOCTYPE html>
+<html lang="ja">
+  <meta charset="utf-8">
+  <title>Redirecting&hellip;</title>
+  <link rel="canonical" href="${path}">
+  <script>location="${path}"</script>
+  <meta http-equiv="refresh" content="0; url=${path}">
+  <meta name="robots" content="noindex">
+  <h1>Redirecting&hellip;</h1>
+  <a href="${path}">Click here if you are not redirected.</a>
+</html>
+`
+
+// find -f **/*.html
+const text = await readFile('./pages.txt', 'utf-8')
+
+const pages = text.split('\n')
+
+for (const path of pages) {
+  const pagePath = `https://blog.imasanari.dev/${path}`.replace(/index\.html$/, '')
+  console.log(pagePath)
+
+  await mkdir(dirname(join('dist', path)), { recursive: true })
+  await writeFile(join('dist', path), template(pagePath))
+}
